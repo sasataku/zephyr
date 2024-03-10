6b02083690cea5f46e03e533fa43f58e06ab3003
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2024-03-10 20:32:44 +0900

    net: context: Fix the ICMP error on udp
    When receiving a UDP packet, net_conn_input() searches for a
    matching connection within `conn_used`.
    
    However, when receiving UDP packets simultaneously from multiple
    clients, we may encounter a situation where the connection that was
    supposed to be bound cannot be found within `conn_used`, and raise
    the ICMP error.
    
    This is because, within recv_udp(), to avoid the failure of
    bind_default(), we temporarily remove it from `conn_used` using
    net_conn_unregister().
    
    If the context already has a connection handler, it means it's
    already registered. In that case, all we have to do is 1) update
    the callback registered in the net_context and 2) update the
    user_data using net_conn_change_callback() and 3) update the
    remote address and port assuming connect() has been called.
    
    Fixes: #70020
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- subsys/net/ip/net_context.c
+++ subsys/net/ip/net_context.c
@@ -2401,9 +2401,29 @@ static int recv_udp(struct net_context *context,
 
 	ARG_UNUSED(timeout);
 
+	/* If the context already has a connection handler, it means it's
+	 * already registered. In that case, all we have to do is 1) update
+	 * the callback registered in the net_context and 2) update the
+	 * user_data using net_conn_change_callback() and 3) update the remote
+	 * address and port assuming connect() has been called.
+	 *
+	 * The callback function passed to net_conn_change_callback() must be
+	 * the same function as the one passed to net_conn_register(), not the
+	 * callback set for the net context passed by recv_udp.
+	 */
 	if (context->conn_handler) {
-		net_conn_unregister(context->conn_handler);
-		context->conn_handler = NULL;
+		context->recv_cb = cb;
+		ret = net_conn_change_callback(context->conn_handler,
+					       net_context_packet_received,
+					       user_data);
+		if (ret) {
+			goto end;
+		}
+		ret = net_conn_change_remote(context->conn_handler,
+					    context->flags & NET_CONTEXT_REMOTE_ADDR_SET ?
+								&context->remote : NULL,
+					    ntohs(net_sin(&context->remote)->sin_port));
+		goto end;
 	}
 
 	ret = bind_default(context);
@@ -2449,6 +2469,7 @@ static int recv_udp(struct net_context *context,
 				user_data,
 				&context->conn_handler);
 
+end:
 	return ret;
 }
 #else
