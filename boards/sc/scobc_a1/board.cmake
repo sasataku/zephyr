e356ac801731b15dfb5841555ba2859e08518b3b
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2025-06-08 15:02:00 +0900

    boards: scobc_a1: Supoprt SWD and flash to NOR
    Supports SWD interface and flash to NOR instead RAM.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- boards/sc/scobc_a1/board.cmake
+++ boards/sc/scobc_a1/board.cmake
@@ -1,5 +1,19 @@
 # SPDX-License-Identifier: Apache-2.0
 
-board_runner_args(openocd "--use-elf" "--config=${BOARD_DIR}/support/openocd-ftdi.cfg")
+if("${SCOBC_A1_USE_FTDI}" STREQUAL "y")
+	set(SCOBC_A1_CFG "openocd-ftdi.cfg")
+else()
+	set(SCOBC_A1_CFG "openocd.cfg")
+endif()
+
+if("${SCOBC_A1_FLASH_RAM}" STREQUAL "y")
+	board_runner_args(openocd
+		"--use-elf"
+		"--config=${BOARD_DIR}/support/${SCOBC_A1_CFG}")
+else()
+	board_runner_args(openocd
+		"--cmd-pre-init=flash bank sc_flash scqspi 0x0000000 0 0 0 cortex_m3.cpu 0x01A00000 0"
+		"--config=${BOARD_DIR}/support/${SCOBC_A1_CFG}")
+endif()
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
